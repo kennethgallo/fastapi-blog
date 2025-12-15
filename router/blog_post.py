7479ed887093dfa715d6184d2fce2dc6f9c0525d
Kenny <kenny@DAV-FWRNQ-M.local>
2025-12-15 15:04:06 -0500

    testing metadata tags

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,10 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Body
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/blog',
     tags=['blog']
 )
 
-@router.post('/new')
-def create_blog():
-    pass
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    num_of_comments: int
+    published: bool | None = None
+
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'id': id,
+        'data': blog.model_dump(),
+        'version': version
+        }
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel, id: int, comment_id: int = Query(None, title='Id of the comment',
+                   description='Some descritopn for comment ID', alias='commentId', deprecated=True), 
+                   content: str = Body(...)):
+    return {'blog': blog, 'id': id, 'comment_id': comment_id, 'content': content}

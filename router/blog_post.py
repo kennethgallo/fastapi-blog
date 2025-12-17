2fbdc1bd109b679c58564f621afb5e79c77f1243
Kenny <kenneth.gallo@mscdirect.com>
2025-12-17 14:35:36 -0500

    Added some validation

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Query, Body
+from typing import List, Optional, Dict
+from fastapi import APIRouter, Query, Body, Path
 from pydantic import BaseModel
 
 router = APIRouter(
@@ -6,11 +7,18 @@ router = APIRouter(
     tags=['blog']
 )
 
+class Image(BaseModel):
+    url: str
+    alias: str
+
 class BlogModel(BaseModel):
     title: str
     content: str
     num_of_comments: int
-    published: bool | None = None
+    published: Optional[bool]
+    tags: List[str] = []
+    metadata: Dict[str, str] = {'key': 'value'}
+    image: Optional[Image] = None
 
 @router.post('/new/{id}')
 def create_blog(blog: BlogModel, id: int, version: int = 1):
@@ -20,8 +28,14 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'version': version
         }
 
-@router.post('/new/{id}/comment')
-def create_comment(blog: BlogModel, id: int, comment_id: int = Query(None, title='Id of the comment',
-                   description='Some descritopn for comment ID', alias='commentId', deprecated=True), 
-                   content: str = Body(...)):
-    return {'blog': blog, 'id': id, 'comment_id': comment_id, 'content': content}
+@router.post('/new/{id}/comment/{comment_id}')
+def create_comment(blog: BlogModel, id: int, 
+                   comment_title: int = Query(None, title='Title of the comment',
+                   description='Some descritopn for comment_title', alias='commentTitle', 
+                   deprecated=True), 
+                   content: str = Body(..., min_length=5, max_length=50, regex='^[a-z\s]*$'),
+                   v: Optional[List[str]] = Query(['v1.0', 'v1.1', 'v1.2']),
+                   comment_id: int = Path(..., gt=5, le=10)
+                   ):
+    return {'blog': blog, 'id': id, 'comment_id': comment_title, 'content': content, 
+            'version': v, 'comment_id': comment_id}

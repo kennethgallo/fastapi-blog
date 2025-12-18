ea3c90cad3ed9fe227f532acd10d9d90061c5e01
Kenny <kenneth.gallo@mscdirect.com>
2025-12-18 10:13:46 -0500

    adding dark mode to docs

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from router import blog_get, blog_post
 
-app = FastAPI()
+# pip install fastapi-swagger-dark
+import fastapi_swagger_dark as fsd
+
+app = FastAPI(docs_url=None)
+
+docs_router = APIRouter()
+fsd.install(
+    docs_router,
+    path="/docs",
+    swagger_ui_parameters={
+        "deepLinking": True,
+        "docExpansion": "list",
+        # Vivid syntax highlighting inside code blocks
+        "syntaxHighlight": {"theme":"dracula"},  
+    },
+)
+app.include_router(docs_router)
+
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
-@app.get('/hello')
+@app.get("/hello")
 def index():
-    return {'message':'Hello World'}
+    return {"message": "Hello World"}

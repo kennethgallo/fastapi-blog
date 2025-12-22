69bb0c99e3a7b17bef42fe70443fc909d2a5754d
Kenny <kenneth.gallo@mscdirect.com>
2025-12-22 08:42:00 -0500

    added db and functionality for creating users

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 
 from fastapi import FastAPI
 from db import models
-from router import blog_get, blog_post
+from router import blog_get, blog_post, user
 from db.database import engine
 
 # pip install dark_swag
 from dark_swag import get_dark_router
 
-# Create ONE FastAPI app, disable default /docs so dark_swag can mount its own.
 app = FastAPI(docs_url=None)
 
-# Mount DarkSwag's router (serves dark-mode Swagger UI and assets at /docs)
 app.include_router(get_dark_router(app))
-
-# Include your application routers
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
-# Example endpoint
+
 @app.get("/hello")
 def index():
     return {"message": "Hello World"}
